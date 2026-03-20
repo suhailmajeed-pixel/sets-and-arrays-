b0213ca8862081b18b7e633199223a2841cce9a4
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-03-20 18:40:35 +0530

    Add files via upload

--- Set_Intersection.py
+++ Set_Intersection.py
@@ -1,18 +1,9 @@
-my_set = {1,2,3}
-print(my_set)
-
-my_set = {1.0 , "hello" , (1,2,3)}
-print (my_set)
-
-my_set = {1,2,3,4,3,2}
-print (my_set)
-
-my_set = set([1,2,3,2])
-print (my_set,"\n")
-
-num_set = set([0,1,3,4,5])
-print ("original set: ")
-print(num_set)
-num_set.pop()
-print ("after romoving the first element from the said set:")
-print (num_set,"\n")
+setx ={"blue","green"} 
+sety ={"yellow","blue"}
+print(setx)
+print(sety)
+print("\nINTERSECTOIN OF TWO SAID SETS: ")
+setz = setx.intersection(sety)
+print(setz)
+setz = setx.union(sety)
+print(setz)
